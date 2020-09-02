8f1fe79749c78263143b84d00988626f93d3b409
Rachid <rachidkamili.pro@gmail.com>
2020-09-01 23:03:44 -0700

    Initial commit

---
+++ app/src/main/java/com/google/mlkit/vision/demo/preference/PreferenceUtils.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2020 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.mlkit.vision.demo.preference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build.VERSION_CODES;
+import android.preference.PreferenceManager;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.annotation.StringRes;
+
+import com.google.android.gms.common.images.Size;
+import com.google.common.base.Preconditions;
+import com.google.mlkit.common.model.LocalModel;
+import com.google.mlkit.vision.demo.CameraSource;
+import com.google.mlkit.vision.demo.CameraSource.SizePair;
+import com.google.mlkit.vision.demo.R;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.google.mlkit.vision.objects.ObjectDetectorOptionsBase.DetectorMode;
+import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions;
+import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions;
+
+/**
+ * Utility class to retrieve shared preferences.
+ */
+public class PreferenceUtils {
+
+    static void saveString(Context context, @StringRes int prefKeyId, @Nullable String value) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(context.getString(prefKeyId), value)
+                .apply();
+    }
+
+    @Nullable
+    public static SizePair getCameraPreviewSizePair(Context context, int cameraId) {
+        Preconditions.checkArgument(
+                cameraId == CameraSource.CAMERA_FACING_BACK
+                        || cameraId == CameraSource.CAMERA_FACING_FRONT);
+        String previewSizePrefKey;
+        String pictureSizePrefKey;
+        if (cameraId == CameraSource.CAMERA_FACING_BACK) {
+            previewSizePrefKey = context.getString(R.string.pref_key_rear_camera_preview_size);
+            pictureSizePrefKey = context.getString(R.string.pref_key_rear_camera_picture_size);
+        } else {
+            previewSizePrefKey = context.getString(R.string.pref_key_front_camera_preview_size);
+            pictureSizePrefKey = context.getString(R.string.pref_key_front_camera_picture_size);
+        }
+
+        try {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+            return new SizePair(
+                    Size.parseSize(sharedPreferences.getString(previewSizePrefKey, null)),
+                    Size.parseSize(sharedPreferences.getString(pictureSizePrefKey, null)));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @RequiresApi(VERSION_CODES.LOLLIPOP)
+    @Nullable
+    public static android.util.Size getCameraXTargetAnalysisSize(Context context) {
+        String prefKey = context.getString(R.string.pref_key_camerax_target_analysis_size);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        try {
+            return android.util.Size.parseSize(sharedPreferences.getString(prefKey, null));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public static FaceDetectorOptions getFaceDetectorOptionsForLivePreview(Context context) {
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
+        float minFaceSize =
+                Float.parseFloat(
+                        sharedPreferences.getString(
+                                context.getString(R.string.pref_key_live_preview_face_detection_min_face_size),
+                                "0.1"));
+
+        FaceDetectorOptions.Builder optionsBuilder =
+                new FaceDetectorOptions.Builder()
+                        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
+                        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_NONE)
+                        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_NONE)
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+                        .setMinFaceSize(minFaceSize);
+        optionsBuilder.enableTracking();
+        return optionsBuilder.build();
+    }
+
+    public static boolean isCameraLiveViewportEnabled(Context context) {
+        return true;
+    }
+}
